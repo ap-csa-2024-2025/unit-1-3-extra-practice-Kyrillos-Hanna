b21446a56aefa20aa6c16f5eae38cba40d5f3a79
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-09-13 11:18:13 -0700

    finished 1.3 hw

--- Gravity.java
+++ Gravity.java
@@ -1,1 +1,12 @@
+public class Gravity {
+    public static void main(String[] args) {
+        final double gravity = 9.8;
+        int time = 23;
+         
+        double height = 0.5 * gravity * time * time;
+        double velocity = gravity * time;
 
+        System.out.println("Cliff Height (meters): " + height);
+        System.out.println("Ball Velocity (meters/sceond): " + velocity);
+    }
+}
